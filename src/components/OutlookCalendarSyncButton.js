0c2bcc5258b049e6055bfec33ce56cea2405a3fb
Maira1432 <60438307+Maira1432@users.noreply.github.com>
2025-06-19 16:50:34 -0500

    OutlookSyncButton

--- src/components/OutlookCalendarSyncButton.js
+++ src/components/OutlookCalendarSyncButton.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import React from 'react';
 import { useMsal } from '@azure/msal-react';
 import { loginRequest } from '../authConfig';
@@ -66,17 +67,18 @@ const OutlookCalendarSyncButton = ({ reservation, onSyncSuccess, onDeleteOutlook
     }
   };
 
-  return (
-    <button
-      onClick={handleSync}
-      className="w-full mt-4 py-2 px-4 bg-orange-500 text-white rounded-lg hover:bg-orange-600 transition-colors flex items-center justify-center space-x-2"
-    >
-      <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path d="M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4ZM5 6H19V8H5V6ZM19 20V10H5V20H19ZM12 13H10V15H12V13ZM14 13H16V15H14V13Z" />
-      </svg>
-      <span>Sincronizar con Outlook Calendar</span>
-    </button>
-  );
+  useEffect(() => {
+    if (
+      reservation &&
+      reservation.date &&
+      reservation.startTime &&
+      reservation.endTime
+    ) {
+      handleSync();
+    }
+  }, []);
+
+  return null;
 };
 
 export default OutlookCalendarSyncButton;
